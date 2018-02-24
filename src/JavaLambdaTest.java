6aa9659776c5b6d7cfc5f161f3476b61b10f9893
Fei <mailpophy@gmail.com>
2018-02-24 00:50:32 -0500

    lambda test

---
+++ src/JavaLambdaTest.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by fei on 2/24/18.
+ */
+public class JavaLambdaTest {
+
+    private static List<Integer> lambdaListTest() {
+        List<Integer> list = new ArrayList<>();
+
+        return list;
+    }
+
+
+}
